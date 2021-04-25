7044cc9a5d6e40db284e08a409e0e1244bb5272f
Ole Liabø <ole.liabo@piql.com>
2021-04-25 20:32:33 +0200

    More complete setup.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,30 @@
+#!/bin/bash
 
+declare -a compilers=($(readlink -f $(dirname $(find .. -iname "ivm64-gcc"))))
+declare -a assemblers=($(readlink -f $(dirname $(find .. -iname "ivm"))))
 
+i=0
+for c in ${compilers[@]}; do
+    echo "$i: $c"
+    i=$((i+1))
+done
+read -p "Select compiler: " c
+IVMCC=${compilers[$c]}
+echo "You selected $IVMCC"
+echo
 
-#IVMCC=$PWD/../ivm-compiler-1.0rc3/gcc-8.3.0-ivm64-1.0rc3/bin/
-##IVMCC=$PWD/../ivm-compiler/gcc-8.3.0-ivm64-1.0rc4/bin/
-IVMCC=$PWD/../ivm-compiler/gcc-8.3.0-ivm64-1.1rc1/bin/
+i=0
+for c in ${assemblers[@]}; do
+    echo "$i: $c"
+    i=$((i+1))
+done
 
-IVM=$PWD/../publish
+read -p "Select assembler: " c
+IVM=${assemblers[$c]}
+echo "You selected $IVM"
 
 export PATH=$PATH:$IVMCC:$IVM
+export TESTDATA_DIR=$PWD/../testdata/reel/png
+
+echo "PATH=$PATH"
+
